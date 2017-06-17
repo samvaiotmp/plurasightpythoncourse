a2f0b629ce9c7f9f314f9bc1b64c2b7f07346e53
samvaio <samvaiotmp@gmail.com>
2017-06-16 23:05:45 -0500

    version 1

--- decisions.py
+++ decisions.py
@@ -8,3 +8,22 @@ a = 1
 b = 2
 
 print("bigger") if a > b else print("smaller")
+
+student_names = ["Mark", "Katrina", "Jessica"]
+
+for name in student_names:
+    print("Student name is {0}".format(name))
+
+x=0
+for index in range(10):
+    x+=10
+    print("The value of X is {0}".format(x))
+x=0
+for index in range(5,10):
+    x+=10
+    print("The value of X is {0}".format(x))
+
+x=0
+for index in range(5,10,2):
+    x+=10
+    print("The value of X is {0}".format(x))
